a26906f7109b8d72b39768f367510d9c7d7fc9b9
degski <degski@gmail.com>
2020-06-08 18:26:04 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,228 @@
+
+// MIT License
+//
+// Copyright (c) 2020 degski
+//
+// Permission is hereby granted, free of charge, to any person obtaining t copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <algorithm>
+#include <initializer_list>
+#include <sax/iostream.hpp>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numeric>
+#include <random>
+#include <span>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <jthread>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+/*
+    -fsanitize = address
+
+    C:\Program Files\LLVM\lib\clang\10.0.0\lib\windows\clang_rt.asan_cxx-x86_64.lib
+    C:\Program Files\LLVM\lib\clang\10.0.0\lib\windows\clang_rt.asan-preinit-x86_64.lib
+    C:\Program Files\LLVM\lib\clang\10.0.0\lib\windows\clang_rt.asan-x86_64.lib
+
+    C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\tbb\lib\intel64_win\vc_mt\tbb.lib
+*/
+
+#include <sax/multi_array.hpp>
+#include <sax/prng_sfc.hpp>
+#include <sax/uniform_int_distribution.hpp>
+
+#if defined( NDEBUG )
+#    define RANDOM 1
+#else
+#    define RANDOM 0
+#endif
+
+namespace ThreadID {
+// Creates t new ID.
+[[nodiscard]] inline int get ( bool ) noexcept {
+    static std::atomic<int> global_id = 0;
+    return global_id++;
+}
+// Returns ID of this thread.
+[[nodiscard]] inline int get ( ) noexcept {
+    static thread_local int thread_local_id = get ( false );
+    return thread_local_id;
+}
+} // namespace ThreadID
+
+namespace Rng {
+// Chris Doty-Humphrey's Small Fast Chaotic Prng.
+[[nodiscard]] inline sax::Rng & generator ( ) noexcept {
+    if constexpr ( RANDOM ) {
+        static thread_local sax::Rng generator ( sax::os_seed ( ), sax::os_seed ( ), sax::os_seed ( ), sax::os_seed ( ) );
+        return generator;
+    }
+    else {
+        static thread_local sax::Rng generator ( sax::fixed_seed ( ) + ThreadID::get ( ) );
+        return generator;
+    }
+}
+} // namespace Rng
+
+#undef RANDOM
+
+sax::Rng & rng = Rng::generator ( );
+
+template<typename T>
+struct reverse_container_wrapper {
+    T & reverse_iterable;
+};
+
+template<typename T>
+auto begin ( reverse_container_wrapper<T> w ) {
+    return std::rbegin ( w.reverse_iterable );
+}
+
+template<typename T>
+auto end ( reverse_container_wrapper<T> w ) {
+    return std::rend ( w.reverse_iterable );
+}
+
+template<typename T>
+reverse_container_wrapper<T> reverse_container_adaptor ( T && reverse_iterable_ ) {
+    return { reverse_iterable_ };
+}
+
+inline constexpr float euler_constant_ps = 2.718'281'746f;
+
+template<int NumRows>
+using avx_float_matrix_type = sax::MatrixCM<float, NumRows, 8, 0, 0>;
+
+template<int NumRows>
+struct avx_float_matrix {
+    avx_float_matrix ( ) noexcept { std::memset ( &data.at ( NumRows - 1, 0 ), 0, 32 ); }
+    alignas ( 32 ) avx_float_matrix_type<NumRows> data;
+};
+
+using float_span = std::span<float>;
+using space      = std::array<float, 1'024>;
+
+void feed_forward ( ) noexcept {}
+
+// spherical family (there exists an efficient algorithm to compute the updates of the output weights irrespective of the output
+// size). The log-Spherical Softmax by Vincent et al. (2015) and the log-Taylor Softmax.
+// In all the experiments, we used hidden layers with rectifiers, whose weights were initialized with a standard deviation in as
+// suggested in He et al. (2015).
+// First, we propose a Parametric Rectified Linear Unit (PReLU) (He '15 e.a).
+// Second, we derive a robust initialization method that particularly considers the rectifier nonlinearities.
+
+[[nodiscard]] float elliotsig_activation ( float net_alpha_ ) noexcept {
+    net_alpha_ /= 1.0f + std::abs ( net_alpha_ ); // branchless after optimization
+    return std::forward<float> ( net_alpha_ );
+}
+[[nodiscard]] float derivative_elliotsig_activation ( float elliotsig_activation_ ) noexcept {
+    elliotsig_activation_ *= elliotsig_activation_;
+    return std::forward<float> ( elliotsig_activation_ );
+}
+
+float top_bit ( float net_alpha_ ) noexcept {
+    int n;
+    std::memcpy ( &n, &net_alpha_, 1 );
+    n >>= 31;
+    net_alpha_ = ( net_alpha_ * n ) + ( 0.0f * net_alpha_ * ( not n ) );
+    return std::forward<float> ( net_alpha_ );
+}
+
+[[nodiscard]] float rectifier_activation ( float net_alpha_ ) noexcept {
+    net_alpha_ *= net_alpha_ >= 0.0f;
+    return std::forward<float> ( net_alpha_ );
+}
+[[nodiscard]] float derivative_rectifier_activation ( float rectifier_activation_ ) noexcept {
+    rectifier_activation_ = rectifier_activation_ >= 0.0f;
+    return std::forward<float> ( rectifier_activation_ );
+}
+[[nodiscard]] float parametric_rectifier_activation ( float net_alpha_, float rectifier_alpha_ ) noexcept {
+    int n;
+    std::memcpy ( &n, &net_alpha_, 1 );
+    n >>= 31;
+    net_alpha_ *= n + rectifier_alpha_ * ( float ) ( ( int ) not( bool ) n );
+    return std::forward<float> ( net_alpha_ );
+}
+[[nodiscard]] float leaky_rectifier_activation ( float net_alpha_ ) noexcept {
+    return parametric_rectifier_activation ( std::forward<float> ( net_alpha_ ), 0.01f );
+}
+[[nodiscard]] float normalized_exponential_function_activation ( float net_alpha_ ) noexcept {
+    net_alpha_ = std::fpow ( euler_constant_ps, net_alpha_ );
+    return std::forward<float> ( net_alpha_ );
+}
+[[nodiscard]] float derivative_function_activation ( float function_activation_ ) noexcept {
+    function_activation_ *= 1.0f - function_activation_;
+    return std::forward<float> ( function_activation_ );
+}
+
+int main ( ) {
+
+    constexpr std::size_t Inp = 4, Out = 2;
+
+    space t, w;
+
+    std::array<float, Out> x = { 0.5f, -0.1f };
+
+    // feed-forward
+    for ( float_span a = { t.data ( ) + Inp, t.size ( ) - Inp }, s = { t.data ( ), Inp }, w = { t.data ( ), Inp };
+          s.end ( ) < a.end ( );
+          a = { a.data ( ) + 1, a.end ( ) }, s = { t.data ( ), s.size ( ) + 1 }, w = { t.data ( ), w.size ( ) + 1 } ) {
+        a[ 0 ] = rectifier_activation ( std::inner_product ( s.begin ( ), s.end ( ), w.begin ( ), 0.0f ) );
+    }
+    // soft-max
+    float_span out{ &*std::prev ( t.end ( ), 2 ), 2 };
+    float max = 0.0f, sum = 0.0f;
+    for ( auto & o : out ) {
+        if ( o > max )
+            max = o;
+        sum += ( o = normalized_exponential_function_activation ( o ) );
+    }
+    for ( auto & o : out )
+        ( ( o /= sum ) += max );
+
+    // back
+
+    float e  = 0.0f;
+    float *p = &*std::next ( out.rbegin ( ) ), *x_ = &*x.rbegin ( );
+
+    for ( auto & o : reverse_container_adaptor ( out ) )
+        e += std::abs ( ( *p-- += derivative_normalized_exponential_function_activation ( o - *x_-- ) ) );
+
+    float_span hid = { t.data ( ) + Inp, out.data ( ) };
+
+    for ( auto & h : reverse_container_adaptor ( hid ) )
+        *p-- += derivative_rectifier_activation ( h );
+
+    return EXIT_SUCCESS;
+}
